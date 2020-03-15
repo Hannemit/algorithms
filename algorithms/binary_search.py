a9f4b87c1305dfd8e9458f8db0f177f7a17df6de
Hannemit <hannehoitzing@gmail.com>
2020-03-15 10:55:27 +0000

    Update binary search, add recursive

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -1,36 +1,59 @@
-def binary_search(array, value) -> int:
+from typing import Union
+import numpy as np
+
+
+def binary_search(array: Union[list, np.ndarray], value: Union[int, float]) -> int:
     """
     Perform binary search to find the index of 'value' in our array. If the value is is not the array, return -1
-    :param array: input array
-    :param value: value we want index of
+    :param array: array-like, input array
+    :param value: scalar, value we want index of
     :return: int, index of value in array, or -1 if value not in array
     """
-    mid = len(array) // 2
+    if len(array) == 0:
+        return -1
+
     left = 0
     right = len(array) - 1
+    mid = (left + right) // 2
 
     if value > array[right] or value < array[left]:
         return -1
 
-    while True:
-        if value < array[mid]:
-            right = mid
-        else:
-            left = mid
-        mid = (right + left) // 2
-
+    while left <= right:
         if array[mid] == value:
             return mid
+        elif value < array[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
 
-        if mid == left:
-            if value == array[right]:
-                return right
+        mid = (right + left) // 2
+    return -1
 
 
-def recursive_binary_search(array, value) -> int:
+def recursive_binary_search(arr: Union[list, np.ndarray], left: int, right: int, value: Union[float, int]) -> int:
     """
-    Perform binary search recursively
-    :param array:
-    :param value:
-    :return:
+    Perform binary search recursively. Returns -1 if value not in array
+    :param arr: array or list, the array we want to search in
+    :param left: int, defines the left boundary of the array we search in. Should be initially set to 0
+    :param right: int, defines the right boundary of the array we search in. Should be initially set to len(arr) - 1
+    :param value: scalar, the value we're looking for
+    :return: int, index of value in array. Return -1 if value not in array
     """
+
+    # this is not necessarily required but saves time if value outside and array very long
+    if len(arr) > 0 and (value < arr[left] or value > arr[right]):
+        return -1
+
+    # do the binary search
+    if right >= left:
+        mid = (left + right) // 2
+
+        if arr[mid] == value:
+            return mid
+        elif arr[mid] > value:
+            return recursive_binary_search(arr, left, mid - 1, value)
+        else:
+            return recursive_binary_search(arr, mid + 1, right, value)
+    else:
+        return -1  # element not present
--- tests/test_binary_search.py
+++ tests/test_binary_search.py
@@ -2,10 +2,17 @@ import pytest
 from algorithms import binary_search
 test_data = [
     ([1, 2, 4, 6, 12, 32, 42, 52, 64, 72, 90, 123, 245], 32, 5),
-    ([2, 3, 4, 5, 6], 2, 0),
-    ([2, 3, 4, 5, 6, 7], 9, -1),
+    ([2, 3, 4.4, 5, 6.8], 2, 0),
     ([2, 3, 4, 5, 6, 7], 7, 5),
-    ([1, 2, 3, 4], 7, -1),
+    ([2, 3, 4, 5, 6, 7.2], 9, -1),
+    ([1, 2, 3.3, 4], 0, -1),
+    ([], 3, -1),
+    ([-10.2, -5, -3, 0, 3, 5, 8], -5, 1),
+    ([-10, -5, -3, 0, 3, 5, 8], -10, 0),
+    ([-10, -5, -3, 0, 3, 5, 8], 3, 4),
+    ([-20, -15, -10, -5, -3, -2, -1], -5, 3),
+    ([-20.643, -10.0, 6.4, 7.6, 8.12], 7.5, -1),
+    ([-20.643, -10.0, 6.4, 7.6, 8.12], 7.6, 3),
 ]
 
 
@@ -15,3 +22,7 @@ def test_binary_search(test_input, value, expected):
     assert out == expected
 
 
+@pytest.mark.parametrize("test_input, value, expected", test_data)
+def test_recursive_binary_search(test_input, value, expected):
+    out = binary_search.recursive_binary_search(test_input, 0, len(test_input) - 1, value)
+    assert out == expected
