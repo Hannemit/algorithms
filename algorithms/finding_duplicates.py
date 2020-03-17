6d80b1ae8eed27e68d339b4128b05e755f32d8c3
Hannemit <hannehoitzing@gmail.com>
2020-03-17 21:32:05 +0000

    Add second finding dup function

--- algorithms/finding_duplicates.py
+++ algorithms/finding_duplicates.py
@@ -2,11 +2,19 @@
 https://medium.com/solvingalgo/solving-algorithmic-problems-find-a-duplicate-in-an-array-3d9edad5ad41
 Site has some good examples of different ways of solving it.
 """
-import numpy as np
+
+"""
+These functions all solve the problems of:
+Given an array of n + 1 integers between 1 and n, find one of the duplicates.
+If there are multiple possible answers, return one of the duplicates.
+If there is no duplicate, return -1.
+"""
 
 
-def find_duplicates_storing(input_list):
+def find_duplicates_storing(input_list: list):
     """
+    Given an input list, return the first duplicate we see. If there are multiple duplicates, we just return one of
+    them. E.g. input list is [1, 2, 2, 3, 3] --> return 2. IF no duplicate found, return -1. Example: [1, 2, 3] --> -1
     This has O(n) time complexity but also O(n) space complexity as we need a second structure to keep track of which
     numbers we've already seen.
     :param input_list:
@@ -22,8 +30,21 @@ def find_duplicates_storing(input_list):
     return -1
 
 
-def find_duplicates_second_method():
-    pass
+def find_duplicates_sorting(input_list: list):
+    """
+    First sort the array, then only check the neighbours. This is O(1) in space complexity since we don't create any
+    extra array to store stuff in, but O(n log n) in time complexity because we have to sort the list up front.
+    :param input_list:
+    :return:
+    """
+    # first sort the input list
+    input_list.sort()
+
+    # then only need to check the element next to the current element
+    for i in range(len(input_list) - 1):
+        if input_list[i] == input_list[i + 1]:
+            return input_list[i]
+    return -1
 
 
 def find_duplicates_third_method():
--- tests/test_finding_duplicates.py
+++ tests/test_finding_duplicates.py
@@ -1,16 +1,24 @@
 import pytest
 from algorithms import finding_duplicates
-import numpy as np
 
 test_data = [
-    ([1, 2, 2, 3], 2),
-    ([1, 3, 4, 2, 3, 5, 2], 3),
-    ([1, 2, 3, 4, 5, 6, 7], -1),
-    ([-2, 2, -3, 0, 9, 4, 0, -5], 0)
+    ([1, 2, 2, 3], [2]),
+    ([1, 3, 4, 2, 3, 5, 2], [3, 2]),
+    ([1, 2, 3, 4, 5, 6, 7], [-1]),
+    ([-2, 2, -3, 0, 9, 4, 0, -5], [0]),
+    ([], [-1]),
+    ([3], [-1]),
+    ([-2, 2, -3, 4, 4.01, 2, 3, 4, 4], [2, 4]),
 ]
 
 
 @pytest.mark.parametrize("inputs, expected", test_data)
-def test_finding_duplicates(inputs, expected):
-    out = finding_duplicates.find_duplicates(inputs)
-    np.testing.assert_allclose(out, expected)
+def test_finding_duplicates_storing(inputs, expected):
+    out = finding_duplicates.find_duplicates_storing(inputs)
+    assert out in expected
+
+
+@pytest.mark.parametrize("inputs, expected", test_data)
+def test_finding_duplicates_sorting(inputs, expected):
+    out = finding_duplicates.find_duplicates_sorting(inputs)
+    assert out in expected
