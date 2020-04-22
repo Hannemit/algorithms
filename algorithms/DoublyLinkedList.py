9b9a38dcb55475c0c6faab8e00fdfbf261349dd5
Hannemit <hannehoitzing@gmail.com>
2020-04-22 08:31:40 +0100

    Add insertion before in doubly linked list

--- algorithms/DoublyLinkedList.py
+++ algorithms/DoublyLinkedList.py
@@ -43,13 +43,14 @@ class DoublyLinkedList:
         current_node.next = new_node
         new_node.prev = current_node
 
-    def insert_after_data(self, after_data, data):
+    def insert_on_data(self, old_data, data, how: str = "after") -> None:
         """
         Insert a new node after the node which has 'after_data' as data.
-        :param after_data: the data (can be anything really) at some node after which we want to insert
-                our new node
+        :param old_data: the data at some node after which (if how = 'after') or before which (if how='before') we want
+                    to insert our new node
         :param data: the data for the new node
-        :return:
+        :param how: string, "before" or "after"
+        :return: None
         """
         if self.head is None:
             print("No data in list")
@@ -57,19 +58,27 @@ class DoublyLinkedList:
 
         current_node = self.head
         while current_node is not None:
-            if current_node.data == after_data:
+            if current_node.data == old_data:
                 break
             current_node = current_node.next
 
         if current_node is None:
             return  # there was no node with 'after_data' data
 
-        new_node = Node(data)
-        new_node.prev = current_node
-        new_node.next = current_node.next
-        if current_node.next is not None:
-            current_node.next.prev = new_node
-        current_node.next = new_node
+        if how == "after":
+            new_node = Node(data)
+            new_node.prev = current_node
+            new_node.next = current_node.next
+            if current_node.next is not None:
+                current_node.next.prev = new_node
+            current_node.next = new_node
+        else:
+            new_node = Node(data)
+            new_node.next = current_node
+            new_node.prev = current_node.prev
+            if current_node.prev is not None:
+                current_node.prev.next = new_node
+            current_node.prev = new_node
 
     def remove_node(self, data_to_remove):
         if self.head is None:
@@ -131,7 +140,7 @@ if __name__ == "__main__":
     llist.remove_node("Monday")
     llist.print_list()
 
-    print("Insert wed night after wed evening and sun morning after Sunday")
-    llist.insert_after_data("wed evening", "wed night")
-    llist.insert_after_data("Sunday", "sun morning")
+    print("Insert tues night before Wednesday and sun morning after Sunday")
+    llist.insert_on_data("Wednesday", "tues evening", how="before")
+    llist.insert_on_data("Sunday", "sun morning", how="after")
     llist.print_list()
