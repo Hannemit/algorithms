92ed1920facb1e9acab6f62c6800397686236329
Hannemit <hannehoitzing@gmail.com>
2020-05-22 09:06:17 +0100

    Add shortest path matrix

--- algorithms/highest_path.py
+++ algorithms/highest_path.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import List, Union
+import queue
 
 
 def find_best_path(mat: List[List[int]]) -> int:
@@ -28,3 +29,58 @@ def find_best_path(mat: List[List[int]]) -> int:
                 mat[i][j] += max(mat[i][j - 1], mat[i - 1][j])
 
     return mat[num_rows - 1][num_cols - 1]
+
+
+def find_shortest_maze_path(
+    mat: List[List[bool]], start: List[int], end: List[int]
+) -> Union[None, int]:
+    # check if start or ending position is a wall
+    if mat[start[0]][start[1]] is True or mat[start[0]][start[1]] is True:
+        return None
+    if len(mat) == 0 or len(mat[0]) == 0:
+        return None  # invalid input matrix
+
+    num_rows = len(mat)
+    num_cols = len(mat[0])
+
+    current = [start[0], start[1]]
+    mat[current[0]][current[1]] = 0
+
+    que = queue.Queue()
+    que.put(current)
+
+    while not que.empty():
+
+        current = que.get()
+
+        left = [current[0], current[1] - 1]
+        right = [current[0], current[1] + 1]
+        up = [current[0] + 1, current[1]]
+        down = [current[0] - 1, current[1]]
+
+        for move in [left, right, up, down]:
+
+            # check whether current element is out of bounds
+            if (
+                move[0] > num_rows - 1
+                or move[0] < 0
+                or move[1] > num_cols - 1
+                or move[1] < 0
+            ):
+                continue
+
+            # if current element is wall (True) or we have already seen it (not False), skip
+            if mat[move[0]][move[1]] is True or mat[move[0]][move[1]] is not False:
+                continue
+
+            # update current state, add one to where we came from
+            mat[move[0]][move[1]] = mat[current[0]][current[1]] + 1
+
+            # add to queue to later look at its neighbours
+            que.put(move)
+
+    # return value at end, or None if end is still unseen (could not reach it from anywhere)
+    end_val = mat[end[0]][end[1]]
+    if end_val is False:
+        return None
+    return end_val
--- tests/test_highest_path.py
+++ tests/test_highest_path.py
@@ -7,8 +7,28 @@ test_input = [
     ([[3]], 3),
 ]
 
+test_input_2 = [
+    (
+        [
+            [False, False, False, False],
+            [True, True, False, True],
+            [False, False, False, False],
+            [False, False, False, False],
+        ],
+        [3, 0],
+        [0, 0],
+        7,
+    ),
+]
+
 
 @pytest.mark.parametrize("input_matrix, expected", test_input)
 def test_highest_path(input_matrix, expected):
     out = highest_path.find_best_path(input_matrix)
+    assert out == expected
+
+
+@pytest.mark.parametrize("input_matrix, start, end, expected", test_input_2)
+def test_find_shortest_maze_path(input_matrix, start, end, expected):
+    out = highest_path.find_shortest_maze_path(input_matrix, start, end)
     assert out == expected
